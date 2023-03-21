14c51632cfae54419b8b2b626fcb31d9776ec844
domenico2003 <domdat03@gmail.com>
2023-03-21 14:43:51 +0100

    setUp

---
+++ assets/js/artistPage.js
@@ -0,0 +1,11 @@
+let amici = document.querySelector(".amici")
+let offCanvas= document.querySelector(".menùAmici")
+let btnClose = document.querySelector(".btn-close")
+amici.onclick=()=>{
+    offCanvas.classList.remove("d-none")
+}
+
+btnClose.onclick=()=>{
+    offCanvas.classList.add("d-none")
+}
+
